c6cfd0ca8bc5b846f1cef217cac62435dbf2ac6f
lagrangepoint2 <lagrangepoint2@gmail.com>
2017-09-30 10:53:00 -0600

    connection updated

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "qbct6vwi8q648mrn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+  host: "qbct6vwi8q648mrn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   user: "uv6ip0foj5h7oong",
   password: "uesywsuhvywd0wkz",
-  database: "`x0vhcig5xba9tn4u`"
+  database: "x0vhcig5xba9tn4u"
 });
 
 connection.connect(function(err) {
